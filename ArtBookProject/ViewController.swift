675ddaf11c5364427a535addc95f24b8e899aa9d
yakupsuda <yakupsuda@gmail.com>
2022-12-05 14:37:06 +0300

    Initial Commit

---
+++ ArtBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBookProject
+//
+//  Created by Yakup Suda on 5.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
